79eb5c90fcfb47beff4663d15fe2251868d83a4d
lee <lee@gmail.com>
2019-01-20 00:41:54 +0800

    update pages

--- src/pages/layout/BasicLayout.js
+++ src/pages/layout/BasicLayout.js
@@ -1,7 +1,8 @@
 import { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout,Icon,Menu } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
 
 class BasicLayout extends Component {
 
@@ -9,10 +10,26 @@ class BasicLayout extends Component {
         return (
             <Layout>
                 <Sider width={256} style={{ minHeight: '100vh', color: 'white' }}>
-                    Sider</Sider>
+                    <div style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px'}}/>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart"></Icon>
+                            <span>helloWord</span>
+                        </Menu.Item>
+                        <SubMenu key="sub1" title={<span><Icon type="dashboard"></Icon>dashboard</span>}>
+                            <Menu.Item key="2">分析页</Menu.Item>
+                            <Menu.Item key="3">监控页</Menu.Item>
+                            <Menu.Item key="4">工作台</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', textAlign: 'center', padding: 0 }}>Header</Header>
-                    <Content style={{ margin: '24px 16px 0' }}>Content</Content>
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                        {this.props.children}
+                        </div>
+                    </Content>
                     <Footer  style={{ textAlign: 'center' }}>Footer</Footer>
                 </Layout>
             </Layout>
